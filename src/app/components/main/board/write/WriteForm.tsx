12712df7fd10ed93c01be976b397acbd2e426504
ksmnsmarts <ksm@nsmarts.co.kr>
2023-07-13 11:59:34 +0900

    업데이트

--- src/app/components/main/board/write/WriteForm.tsx
+++ src/app/components/main/board/write/WriteForm.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link';
 import { writePost } from '@/services/board/writePost';
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
+import Image from 'next/image';
 
 type Form = {
     title: string,
@@ -66,20 +67,21 @@ export default function WriteForm() {
 
 
 
-    return <section className='flex justify-center items-center w-screen'>
+    return <section className='flex justify-center items-center w-screen pt-[95px]'>
         <div>
-            <form onSubmit={onSubmit} className='w-full flex flex-col gap-2 my-4 p-4 bg-slate-700 rounded-xl text-white'>
+            <form onSubmit={onSubmit} className='flex flex-col border p-[50px] bg-white rounded-[40px] drop-shadow-xl lg:w-[500px] lg:h-[600px]'>
+                <Image src="/images/code_write.png" width={50} height={50} alt="logo" className='m-auto' />
                 {/* <p className='text-center'>게시글</p> */}
                 <span className='w-full border-2'></span>
                 {/* title */}
                 <label htmlFor="title" className='font-semibold'>제목</label>
-                <input className='text-black' type="text" id='title' name="title" required autoFocus value={form.title} onChange={onChange} />
+                <input className='bg-gray-200 shadow-inner rounded-l p-2 flex-1' type="text" id='title' name="title" required autoFocus value={form.title} onChange={onChange} />
                 {/* contents1 */}
                 <label htmlFor="contents1" className='font-semibold'>항목</label>
-                <input className='text-black' type="text" id='contents1' name="contents1" required autoFocus value={form.contents1} onChange={onChange} />
+                <input className='bg-gray-200 shadow-inner rounded-l p-2 flex-1' type="text" id='contents1' name="contents1" required autoFocus value={form.contents1} onChange={onChange} />
                 {/* contents2 */}
                 <label htmlFor="contents2" className='font-semibold'>내용</label>
-                <textarea className='text-black' id='contents2' name='contents2' required autoFocus value={form.contents2} onChange={onChange} />
+                <textarea className='bg-gray-200 shadow-inner rounded-l p-2 flex-1' id='contents2' name='contents2' required autoFocus value={form.contents2} onChange={onChange} />
                 <button className='bg-yellow-300 text-black font-bold mt-5 hover:bg-yellow-400'>작성</button>
                 <Link href='/main/board' className='bg-green-300 text-black font-bold hover:bg-green-400 text-center'>돌아가기</Link>
             </form>
